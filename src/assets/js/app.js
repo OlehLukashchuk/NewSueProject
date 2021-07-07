7c89245ee4d83ba79dea6c91f517cbbb42f58fec
Tempest96 <eldorarempest@gmail.com>
2021-07-07 17:48:18 +0300

    codeOptimiz

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,30 +1,40 @@
 let catalogButton = document.getElementById("catalog");
-let catalogBlock = document.querySelector(".header__subnav");
+let catalogNav = document.querySelector(".subnav__container");
 
 catalogButton.addEventListener("mouseover", (event) => {
   event.target.parentElement.style.borderBottom = "4px solid #888440";
-  catalogBlock.style.height = "27rem";
+  catalogNav.style.height = "27rem";
 });
 
 $(".header__subnav").on("mouseleave", (event) => {
   catalogButton.parentElement.style.borderBottom = "4px solid transparent";
-  catalogBlock.style.height = 0;
+  catalogNav.style.height = 0;
 });
 
-
 $(document).ready(() => {
-  $('.slider').slick({
-    adaptiveHeight : true,
-    slidesToShow : 4,
-    slidesToScroll : 1,
-    speed :500,
-    easing :'ease',
-    autoplay :true,
-    infinite:true,
-    autoplaySpeed :1500,
+  $(".slider1").slick({
+    adaptiveHeight: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 500,
+    easing: "ease",
+    autoplay: true,
+    infinite: true,
+    autoplaySpeed: 1500,
     pauseOnFocus: true,
     pausedOnHover: true,
-    touchThreshold : 10
-
+    touchThreshold: 10,
   });
-})
+  $(".slider2").slick({
+    adaptiveHeight: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 500,
+    easing: "ease",
+    infinite: true,
+    autoplaySpeed: 1500,
+    pauseOnFocus: true,
+    pausedOnHover: true,
+    touchThreshold: 10,
+  });
+});
