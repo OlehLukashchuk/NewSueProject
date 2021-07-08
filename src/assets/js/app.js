3f7968cbf70e018ba44208cd7de6c3e41b1ea6b9
Tempest96 <eldorarempest@gmail.com>
2021-07-08 15:39:42 +0300

    mobileAdaptive

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,15 +1,26 @@
+/* DOM */
 let catalogButton = document.getElementById("catalog");
 let mobileCatalogButton = document.querySelectorAll(".mobile-catalog");
 let aboutButton = document.getElementById("about");
 let mobileAboutButton = document.querySelectorAll(".mobile-about");
+let productButton = document.querySelectorAll(".product-element__button");
 
 let catalogNav = document.querySelector(".subnav1");
 let mobileCatalog = document.querySelector(".subnav-catalog");
 let aboutNav = document.querySelector(".subnav2");
-let mobileAbout= document.querySelector(".subnav-about");
+let mobileAbout = document.querySelector(".subnav-about");
 
-let mobileMenu = document.querySelector('.mobile-menu');
 let burger = document.querySelector(".burger-menu");
+let mobileMenu = document.querySelector(".mobile-menu");
+let productMobile = document.querySelector(".products-mobile");
+
+/* Slider */
+// $(document).ready(() => {
+
+// });
+/* Slider */
+
+/* Events */
 
 catalogButton.addEventListener("mouseover", (event) => {
   event.target.parentElement.style.borderBottom = "4px solid #888440";
@@ -29,15 +40,58 @@ $(".header__subnav").on("mouseleave", (event) => {
   catalogNav.style.height = 0;
   aboutNav.style.height = 0;
 });
+/* Events */
 
-$(document).ready(() => {
+/* MOBILE */
+
+if (
+  navigator.userAgent.match(/Android/i) ||
+  navigator.userAgent.match(/webOS/i) ||
+  navigator.userAgent.match(/iPhone/i) ||
+  navigator.userAgent.match(/iPad/i) ||
+  navigator.userAgent.match(/iPod/i) ||
+  navigator.userAgent.match(/BlackBerry/i) ||
+  navigator.userAgent.match(/Windows Phone/i)
+) {
+  productMobile.style.display = "block";
+  for (let i = 0; i < productButton.length; i++) {
+    productButton[i].innerText = "В корзину";
+  }
+
+  $(".slider1").slick({
+    adaptiveHeight: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 500,
+    easing: "ease",
+    // autoplay: true,
+    infinite: true,
+    autoplaySpeed: 1500,
+    pauseOnFocus: true,
+    pausedOnHover: true,
+    touchThreshold: 10,
+  });
+
+  $(".slider2").slick({
+    adaptiveHeight: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 500,
+    easing: "ease",
+    infinite: true,
+    autoplaySpeed: 1500,
+    pauseOnFocus: true,
+    pausedOnHover: true,
+    touchThreshold: 10,
+  });
+} else {
   $(".slider1").slick({
     adaptiveHeight: true,
     slidesToShow: 4,
     slidesToScroll: 1,
     speed: 500,
     easing: "ease",
-    autoplay: true,
+    // autoplay: true,
     infinite: true,
     autoplaySpeed: 1500,
     pauseOnFocus: true,
@@ -56,12 +110,10 @@ $(document).ready(() => {
     pausedOnHover: true,
     touchThreshold: 10,
   });
-});
-
-/* MOBILE */
+}
 
 burger.addEventListener("click", () => {
-  mobileMenu.classList.toggle('_open');
+  mobileMenu.classList.toggle("_open");
   burger.classList.toggle("_active");
 });
 
@@ -78,3 +130,5 @@ for (let i = 0; i < mobileAboutButton.length; i++) {
     mobileAboutButton[1].classList.toggle("_open");
   });
 }
+
+/* MOBILE */
